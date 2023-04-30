96f655a01b4c9556e61a808ec822ce050de03cee
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-04-30 05:42:18 -0500

    Adding 02-Sending-Text-to-Output files and directories

---
+++ 02-Sending-Text-to-Output/main.c
@@ -0,0 +1,16 @@
+/*
+    Author: Matt Johnson
+    Subject: C basics
+    purpose: program prints "Thrilled to meet you!"
+    Date: 04/30/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    puts("Thrilled to meet you!");
+
+    return 0;
+}
