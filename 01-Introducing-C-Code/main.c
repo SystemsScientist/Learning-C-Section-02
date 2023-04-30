962d73866f528e0480cd2b28833257bf277abed1
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-04-30 05:24:46 -0500

    Adding 01-Introducing-C-Code files and directories

---
+++ 01-Introducing-C-Code/main.c
@@ -0,0 +1,17 @@
+/*
+    Author: Matt Johnson
+    Subject: C basics
+    purpose: program executes the simplest C program,
+             which is called the dummy program
+    Date: 04/30/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    // program produces no output
+
+    return 0; // can use any integer: ..., -3, -2, -1, 0, 1, 2, 3, ...
+}
