9661a5542fc4b2c57463f9727803f893fa440d50
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-04-30 05:52:28 -0500

    Adding 03-Add-puts-Statement files and directories

---
+++ 03-Add-puts-Statement/main.c
@@ -0,0 +1,17 @@
+/*
+    Author: Matt Johnson
+    Subject: C basics
+    purpose: program prints two puts functions
+    Date: 04/30/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    puts("Thrilled to meet you!");
+    puts("This is pretty awesome!");
+
+    return 0;
+}
