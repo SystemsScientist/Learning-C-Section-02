54cd7ca6bd2c22462ade4b9dc8a9a5fcbca598b7
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-01 05:01:52 -0500

    Adding 06-Exploring-the-printf-Function files and directories

---
+++ 06-Exploring-the-printf-Function/main.c
@@ -0,0 +1,17 @@
+/*
+    Author: Matt Johnson
+    Subject: C basics
+    purpose: program utilizes the printf() function
+    Date: 04/30/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    printf("This is the way the world ends.\n"); // adds a newline sequence
+    printf("Not with a bang but a whimper.\n");  // adds a newline sequence
+
+    return 0;
+}
