e4502b4df825f2a203209c64b3f79bf65840f487
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-01 04:47:18 -0500

    Adding 05-Adding-Comments-to-the-Code files and directories

---
+++ 05-Adding-Comments-to-the-Code/main.c
@@ -0,0 +1,17 @@
+/*
+    Author: Matt Johnson
+    Subject: C basics
+    purpose: program uses single-line and multi-line comments
+    Date: 04/30/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    printf("This program uses single line comments!\n"); // single line comment
+    printf("This program also uses multi-line comments.\n");
+
+    return 0;
+}
