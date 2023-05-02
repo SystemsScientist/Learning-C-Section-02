abd528092a8e362c656cc52e8557336a4fb10ad7
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-02 05:08:04 -0500

    Adding 10-Challenge-Use Escape Sequences files and directories

---
+++ 10-Challenge-Use-Escape-Sequences/main.c
@@ -0,0 +1,28 @@
+/*
+    Author: Matt Johnson
+    Subject: C basics
+    purpose: program provides challenge details
+    Date: 05/02/2023
+
+    Requirements
+
+        1. Create a new source code file with a single
+           printf() statement in the main() function
+
+        2. The statement must generate two lines of output
+           and include one word enclosed in double quotes
+
+        3. Save the source code as a .c file
+
+        4. Compile, build, and run program to test
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    // program displays no output
+
+    return 0;
+}
