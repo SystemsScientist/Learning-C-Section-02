d2c7d70daee819382bb4e2aea381194298f8ec68
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-02 05:12:03 -0500

    Adding 11-Solution-Use-Escape-Sequences files and directories

---
+++ 11-Solution-Use-Escape-Sequences/main.c
@@ -0,0 +1,28 @@
+/*
+    Author: Matt Johnson
+    Subject: C basics
+    purpose: program provides challenge details
+    Date: 05/02/2023
+
+    Requirements
+
+        1. Create a new source code file with a single
+           printf() statement in the main() function
+
+        2. The statement must generate two lines of output
+           and include one word enclosed in double quotes
+
+        3. Save the source code as a .c file
+
+        4. Compile, build, and run program to test
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    printf("This is the first line.\nThis is the \"second\" line.\n");
+
+    return 0;
+}
