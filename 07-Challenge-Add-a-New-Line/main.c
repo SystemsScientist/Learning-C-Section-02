ebcc2f2a5a3fff1e9cf2f09e53fcb9031eb3b39f
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-02 04:41:48 -0500

    07-Challenge-Add-a-New-Line files and directories

---
+++ 07-Challenge-Add-a-New-Line/main.c
@@ -0,0 +1,28 @@
+/*
+    Author: Matt Johnson
+    Subject: C basics
+    purpose: program provides challenge details
+    Date: 04/30/2023
+
+    Requirements
+
+        1. Create a new source code file or project
+
+        2. Add a single printf() statement
+
+        3. The statement must generate two lines of output
+
+        4. Save the source code as a .c file
+
+        5. Compile, build, and run program
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    // program displays no output
+
+    return 0;
+}
